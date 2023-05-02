b4680db2b3536996ec47b7205499348fb68d9c46
Yash <imyashpawar@gmail.com>
2023-05-02 15:35:36 +0530

    Added

---
+++ src/com/brdigelabz/sorting/BinarySearch.java
@@ -0,0 +1,13 @@
+package com.brdigelabz.sorting;
+
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+        String arr1 = "Banana Apple Dragon Fruit Kiwi Peach";
+        Arrays.toString(arr1.toCharArray());
+
+
+    }
+}
--- src/com/brdigelabz/sorting/BubbleSort.java
+++ src/com/brdigelabz/sorting/BubbleSort.java
@@ -1,6 +1,4 @@
-package com.brdigelabz.bubblesort;
-
-import java.util.ArrayList;
+package com.brdigelabz.sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
